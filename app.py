7319cc97e6469279a5b3877f0781b6b512d9acf4
sameerk007 <rokzsam007@gmail.com>
2024-03-21 11:03:52 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def main():
+    st.title("Welcome to my edition of Streamlit")
+    st.write("Just Testing some stuff on Azure!")
+
+if __name__ == "__main__":
+    main()
